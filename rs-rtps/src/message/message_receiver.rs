8ca407e6245314f14552c383643a6523e362f301
tomiy <tomiy@tomiylab.com>
2023-03-22 14:06:55 +0900

    implement method of message_receiver

--- rs-rtps/src/message/message_receiver.rs
+++ rs-rtps/src/message/message_receiver.rs
@@ -6,6 +6,7 @@ use bytes::Bytes;
 
 
 pub struct MessageReceiver {
+    own_guidPrefix: GuidPrefix,
     sourceVersion: ProtocolVersion,
     sourceVendorId: VendorId,
     sourceGuidPrefix: GuidPrefix,
@@ -19,10 +20,11 @@ pub struct MessageReceiver {
 impl MessageReceiver {
     pub fn new(participant_guidprefix: GuidPrefix) -> MessageReceiver {
         Self {
+            own_guidPrefix: participant_guidprefix,
             sourceVersion: ProtocolVersion::PROTOCOLVERSION,
             sourceVendorId: VendorId::VENDORID_UNKNOW,
             sourceGuidPrefix: GuidPrefix::UNKNOW,
-            destGuidPrefix: participant_guidprefix,
+            destGuidPrefix: GuidPrefix::UNKNOW,
             unicastReplyLocatorList: vec!(Locator::INVALID),
             multicastReplyLocatorList: vec!(Locator::INVALID),
             haveTimestamp: false,
@@ -30,7 +32,18 @@ impl MessageReceiver {
         }
     }
 
-    pub fn handle_packet(&self, packets: Vec<UdpMessage>) {
+    fn reset(&mut self) {
+        self.sourceVersion = ProtocolVersion::PROTOCOLVERSION;
+        self.sourceVendorId = VendorId::VENDORID_UNKNOW;
+        self.sourceGuidPrefix = GuidPrefix::UNKNOW;
+        self.destGuidPrefix = GuidPrefix::UNKNOW;
+        self.unicastReplyLocatorList.clear();
+        self.multicastReplyLocatorList.clear();
+        self.haveTimestamp = false;
+        self.timestamp = Timestamp::TIME_INVALID;
+    }
+
+    pub fn handle_packet(&mut self, packets: Vec<UdpMessage>) {
         for packet in packets {
             // Is DDSPING
             let msg = packet.message;
@@ -52,16 +65,26 @@ impl MessageReceiver {
             // bufの4からoctetsToNextHeaderをSubMessageBodyにシリアライズ
             // Vecに突っ込む
             let mut submessages: Vec<SubMessage> = Vec::new();
+            let sub_header_lenght = 4;
             while !rtps_body_buf.is_empty() {
                 let submessage_header_buf = rtps_body_buf.split_to(4);
                 // TODO: Message Receiverが従うルール (spec 8.3.4.1)に沿った実装に変更
                 // 不正なsubmessageを受信した場合、残りのMessageは無視
-                // Messageの拡張の実装
                 let submessage_header = match SubMessageHeader::read_from_buffer(&submessage_header_buf) {
                     Ok(h) => h,
                     Err(_) => break,
                 };
-                let submessage_body_buf = rtps_body_buf.split_to(submessage_header.get_len() as usize);
+                // RTPS spec 2.3, section 9.4.5.1.3
+                let submessage_body_len = if submessage_header.get_content_len() == 0 {
+                    match submessage_header.get_submessagekind() {
+                        SubMessageKind::PAD | SubMessageKind::INFO_TS => 0,
+                        _ => rtps_body_buf.len(),
+                    }
+                } else {
+                    submessage_header.get_content_len() as usize
+                };
+
+                let submessage_body_buf = rtps_body_buf.split_to(submessage_body_len);
                 let submessage = SubMessage::new(submessage_header, submessage_body_buf);
                 match submessage {
                     Some(msg) => submessages.push(msg),
@@ -73,8 +96,10 @@ impl MessageReceiver {
         }
     }
 
-    fn handle_parsed_packet(&self, rtps_msg: Message){
-        // TODO: 自身のメンバー変数をメッセージのヘッダに従って更新
+    fn handle_parsed_packet(&mut self, rtps_msg: Message){
+        self.reset();
+        self.destGuidPrefix = self.own_guidPrefix;
+        self.sourceGuidPrefix = rtps_msg.header.guidPrefix;
         println!(">>>>>>>>>>>>>>>>");
         println!("header: {:?}", rtps_msg.header);
         for submsg in rtps_msg.submessages {
