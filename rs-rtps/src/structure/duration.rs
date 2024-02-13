b6b3f867923f6f97bf61c07e581623608e1d4432
tomiy <tomiy@tomiylab.com>
2024-02-13 16:00:56 +0900

    implement new() for structure::duration::Duration

--- rs-rtps/src/structure/duration.rs
+++ rs-rtps/src/structure/duration.rs
@@ -5,3 +5,9 @@ pub struct Duration {
     pub seconds: i32,
     pub fraction: u32,
 }
+
+impl Duration {
+    pub fn new(seconds: i32, fraction: u32) -> Self {
+        Self { seconds, fraction }
+    }
+}
