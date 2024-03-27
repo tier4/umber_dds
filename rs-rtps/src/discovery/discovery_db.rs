3195cb19d14358158ce34d356278db34c73a9a4c
tomiy <tomiy@tomiylab.com>
2024-03-27 17:11:51 +0900

    implement DiscoveryDB

--- rs-rtps/src/discovery/discovery_db.rs
+++ rs-rtps/src/discovery/discovery_db.rs
@@ -1,1 +1,62 @@
-pub struct DiscoveryDB {/* TODO */}
+use crate::discovery::structure::data::SPDPdiscoveredParticipantData;
+use crate::message::submessage::element::Timestamp;
+use crate::structure::duration::Duration;
+use crate::structure::guid::GuidPrefix;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
+pub struct DiscoveryDB {
+    inner: Arc<Mutex<DiscoveryDBInner>>,
+}
+impl DiscoveryDB {
+    pub fn new() -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(DiscoveryDBInner::new())),
+        }
+    }
+
+    pub fn write(
+        &mut self,
+        guid_prefix: GuidPrefix,
+        timestamp: Timestamp,
+        data: SPDPdiscoveredParticipantData,
+    ) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.write(guid_prefix, timestamp, data)
+    }
+
+    fn read(&self, guid_prefix: GuidPrefix) -> Option<SPDPdiscoveredParticipantData> {
+        let mut inner = self.inner.lock().unwrap();
+        inner.read(guid_prefix)
+    }
+}
+
+struct DiscoveryDBInner {
+    data: HashMap<GuidPrefix, (Timestamp, SPDPdiscoveredParticipantData)>,
+}
+
+impl DiscoveryDBInner {
+    fn new() -> Self {
+        Self {
+            data: HashMap::new(),
+        }
+    }
+
+    fn write(
+        &mut self,
+        guid_prefix: GuidPrefix,
+        timestamp: Timestamp,
+        data: SPDPdiscoveredParticipantData,
+    ) {
+        self.data.insert(guid_prefix, (timestamp, data));
+    }
+
+    fn read(&self, guid_prefix: GuidPrefix) -> Option<SPDPdiscoveredParticipantData> {
+        if let Some((_ts, data)) = self.data.get(&guid_prefix) {
+            Some((*data).clone())
+        } else {
+            None
+        }
+    }
+}
