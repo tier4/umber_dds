ce999d92ea1caf0befa3e2a421b2898cfbfab820
tomiy <tomiy@tomiylab.com>
2024-05-14 16:54:42 +0900

    change heartbeat_period wheather writer is reliable or not

--- rs-rtps/src/dds/publisher.rs
+++ rs-rtps/src/dds/publisher.rs
@@ -140,7 +140,9 @@ impl InnerPublisher {
     ) -> DataWriter<D> {
         let (writer_command_sender, writer_command_receiver) =
             mio_channel::sync_channel::<WriterCmd>(4);
+        let mut heartbeat_period = Duration::ZERO;
         let reliability_level = if let Some(reliability) = qos.reliability {
+            heartbeat_period = Duration::new(20, 0);
             reliability.kind
         } else {
             ReliabilityQosKind::BestEffort // If qos don't specify reliability_level, the
@@ -153,7 +155,7 @@ impl InnerPublisher {
             unicast_locator_list: Vec::new(),
             multicast_locator_list: Vec::new(),
             push_mode: false,
-            heartbeat_period: Duration::new(20, 0),
+            heartbeat_period,
             nack_response_delay: Duration::new(0, 200 * 1000 * 1000),
             nack_suppression_duration: Duration::ZERO,
             data_max_size_serialized: 0,
