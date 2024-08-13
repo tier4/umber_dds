3a8f7aabdc9627cc620d9805d08970793b92d9c9
tomiy <tomiy@tomiylab.com>
2024-08-13 15:19:02 +0900

    update Gap Submessage

--- rs-rtps/src/message/submessage/element/gap.rs
+++ rs-rtps/src/message/submessage/element/gap.rs
@@ -4,17 +4,34 @@ use crate::structure::entity_id::*;
 use enumflags2::BitFlags;
 use speedy::{Readable, Writable};
 
-#[derive(Readable, Writable, Clone)]
+#[derive(Readable, Clone)]
 pub struct Gap {
     pub reader_id: EntityId,
     pub writer_id: EntityId,
     pub gap_start: SequenceNumber,
     pub gap_list: SequenceNumberSet,
-    pub gap_start_gsn: SequenceNumber,
-    pub gap_end_gsn: SequenceNumber,
+    #[speedy(default_on_eof)]
+    pub gap_start_gsn: Option<SequenceNumber>,
+    #[speedy(default_on_eof)]
+    pub gap_end_gsn: Option<SequenceNumber>,
 }
 
 impl Gap {
+    pub fn new(
+        reader_id: EntityId,
+        writer_id: EntityId,
+        gap_start: SequenceNumber,
+        gap_list: SequenceNumberSet,
+    ) -> Self {
+        Self {
+            reader_id,
+            writer_id,
+            gap_start,
+            gap_list,
+            gap_start_gsn: None,
+            gap_end_gsn: None,
+        }
+    }
     pub fn is_valid(&self, flag: BitFlags<GapFlag>) -> bool {
         // rtps 2.3 spec 8.3.7.4 Gap
         // validation
@@ -30,22 +47,56 @@ impl Gap {
         }
         if flag.contains(GapFlag::GroupInfo) {
             // GroupInfoFlag is set and
-            if self.gap_start_gsn <= SequenceNumber(0) {
-                // gapStartGSN.value is zero or negative
-                eprintln!("Invalid Gap Submessage received");
-                return false;
+            match self.gap_start_gsn {
+                Some(gs_gsn) => {
+                    if gs_gsn <= SequenceNumber(0) {
+                        // gapStartGSN.value is zero or negative
+                        eprintln!("Invalid Gap Submessage received");
+                        return false;
+                    }
+                }
+                None => {
+                    eprintln!("Invalid Gap Submessage received");
+                    return false;
+                }
             }
-            if self.gap_end_gsn <= SequenceNumber(0) {
-                // gapEndGSN.value is zero or negative
-                eprintln!("Invalid Gap Submessage received");
-                return false;
-            }
-            if self.gap_end_gsn < self.gap_start_gsn - SequenceNumber(1) {
-                // gapEndGSN.value < gapStartGSN.value-1
-                eprintln!("Invalid Gap Submessage received");
-                return false;
+            match self.gap_end_gsn {
+                Some(ge_gsn) => {
+                    if ge_gsn <= SequenceNumber(0) {
+                        // gapEndGSN.value is zero or negative
+                        eprintln!("Invalid Gap Submessage received");
+                        return false;
+                    }
+                    if ge_gsn < self.gap_start_gsn.unwrap() - SequenceNumber(1) {
+                        // gapEndGSN.value < gapStartGSN.value-1
+                        eprintln!("Invalid Gap Submessage received");
+                        return false;
+                    }
+                }
+                None => {
+                    eprintln!("Invalid Gap Submessage received");
+                    return false;
+                }
             }
         }
         true
+    }
+}
+
+impl<C: Context> Writable<C> for Gap {
+    fn write_to<T: ?Sized + Writer<C>>(&self, writer: &mut T) -> Result<(), C::Error> {
+        writer.write_value(&self.reader_id)?;
+        writer.write_value(&self.writer_id)?;
+        writer.write_value(&self.gap_start)?;
+        writer.write_value(&self.gap_list)?;
+        match self.gap_start_gsn {
+            Some(gs_gsn) => writer.write_value(&gs_gsn)?,
+            None => (),
+        }
+        match self.gap_end_gsn {
+            Some(ge_gsn) => writer.write_value(&ge_gsn)?,
+            None => (),
+        }
+        Ok(())
     }
 }
