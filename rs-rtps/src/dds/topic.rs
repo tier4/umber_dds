5d1d026a47e1d70433a6fb2910bed34df2d76432
tomiy <tomiy@tomiylab.com>
2024-08-21 16:48:02 +0900

    add some QoS policies to sub_builtin_topic_data

--- rs-rtps/src/dds/topic.rs
+++ rs-rtps/src/dds/topic.rs
@@ -105,26 +105,22 @@ impl InnerTopic {
     }
     fn sub_builtin_topic_data(&self) -> SubscriptionBuiltinTopicData {
         /*
-         * if SubscriptionBuiltinTopicData include Some QoS Policy,
-         * FastDDS throw error and couldn't match.
-         * ```
-         * QObject::connect: Cannot queue arguments of type 'QTextCursor'
-         * (Make sure 'QTextCursor' is registered using qRegisterMetaType().)
-         * ```
-         * TOOD: survey this problem
+         * if SubscriptionBuiltinTopicData include liveliness and deadline QoS Policy,
+         * FastDDS say detects incompatible QoS.
+         * TODO: fix it
          */
         SubscriptionBuiltinTopicData::new(
             None,
             None,
             Some(self.name.clone()),
             Some(self.type_desc.clone()),
-            None, // Some(self.my_qos_policies.durability),
+            Some(self.my_qos_policies.durability),
             None, // Some(self.my_qos_policies.deadline),
-            None, // Some(self.my_qos_policies.latency_budget),
+            Some(self.my_qos_policies.latency_budget),
             None, // Some(self.my_qos_policies.liveliness),
             Some(self.my_qos_policies.reliability),
-            None, // Some(self.my_qos_policies.ownership),
-            None, // Some(self.my_qos_policies.destination_order),
+            Some(self.my_qos_policies.ownership),
+            Some(self.my_qos_policies.destination_order),
             None,
             None,
             None,
@@ -132,7 +128,7 @@ impl InnerTopic {
             None,
             None,
             None,
-            None, // Some(self.my_qos_policies.lifespan),
+            Some(self.my_qos_policies.lifespan),
         )
     }
 }
