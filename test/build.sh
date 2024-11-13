398a90c29d5045c8b7109744f71d0ca85f80e150
tomiy <tomiy@tomiylab.com>
2024-11-13 16:43:18 +0900

    update test env

--- test/build.sh
+++ test/build.sh
@@ -3,7 +3,7 @@
 if [ "$(sudo docker image ls -q "fastdds_img")" ]; then
     echo fastdds_img is already exist
 else
-    sudo docker build ../../test/fastdds_img --network host -t fastdds_img
+    sudo docker build ./fastdds_img --network host -t fastdds_img
 fi
 
 sudo docker compose build
