e19f935bd6be318d08319218a13f86775977a30f
tomiy <tomiy@tomiylab.com>
2025-08-01 14:51:04 +0900

    fix wrong definition of InfoSoruce message

--- src/message/submessage/element/infosrc.rs
+++ src/message/submessage/element/infosrc.rs
@@ -4,6 +4,7 @@ use speedy::{Readable, Writable};
 
 #[derive(Readable, Writable)]
 pub struct InfoSource {
+    unused: i32,
     pub protocol_version: ProtocolVersion,
     pub vendor_id: VendorId,
     pub guid_prefix: GuidPrefix,
