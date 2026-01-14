5fdb77fa122c15170a07350228f5bfaff5c755ce
tomiy <tomiy@tomiylab.com>
2026-01-14 15:03:26 +0900

    udpate test_case*.sh: log the exit codes of each process

--- test/test_cases/test_case1.sh
+++ test/test_cases/test_case1.sh
@@ -4,16 +4,26 @@ sudo docker compose up -d
 
 declare -a pids=()
 declare -i results=0
+declare -A proc_labels
 
 sudo docker exec -i umber_dds /home/umber_dds/target/debug/examples/shapes_demo_for_autotest -m s &
-pids+=($!)
+pid1=$!
+pids+=($pid1)
+proc_labels[$pid1]=" shapes_demo_for_autotest (umber_dds, Sub)"
 
 sudo docker exec -i otherdds /home/shapes_demo_rustdds/target/debug/shapes_demo_rustdds -t Square -P &
-pids+=($!)
+pid2=$!
+pids+=($pid2)
+proc_labels[$pid2]="shapes_demo_rustdds (rustdds, Pub)"
 
 for pid in "${pids[@]}"; do
     wait "$pid"
-    results+=$?
+    exit_code=$?
+
+    label=${proc_labels[$pid]}
+    echo "[Exit Code: $exit_code] - $label"
+
+    results+=$exit_code
 done
 
 exit $results
--- test/test_cases/test_case5.sh
+++ test/test_cases/test_case5.sh
@@ -4,16 +4,26 @@ sudo docker compose up -d
 
 declare -a pids=()
 declare -i results=0
+declare -A proc_labels
 
 sudo docker exec -i otherdds /home/shapes_demo_cyclonedds/ShapesDemoSubscriber &
-pids+=($!)
+pid1=$!
+pids+=($pid1)
+proc_labels[$pid1]="ShapesDemoSubscriber (cyclonedds, Sub)"
 
 sudo docker exec -i umber_dds /home/umber_dds/target/debug/examples/shapes_demo_for_autotest -m p &
-pids+=($!)
+pid2=$!
+pids+=($pid2)
+proc_labels[$pid2]="shapes_demo_rustdds (umber_dds, Pub)"
 
 for pid in "${pids[@]}"; do
     wait "$pid"
-    results+=$?
+    exit_code=$?
+
+    label=${proc_labels[$pid]}
+    echo "[Exit Code: $exit_code] - $label"
+
+    results+=$exit_code
 done
 
 exit $results
